0985a59663f564b10339c9c81250d7b80a51b9f4
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-09-07 22:30:31 +0530

    Create pgm1.py

---
+++ pgm1.py
@@ -0,0 +1,11 @@
+myDictionary = {
+	"name":"archie",
+	"identity":"student",
+	"age":17
+}
+print(myDictionary)
+ 
+key=myDictionary["name"]
+value=myDictionary.get("name")
+print("key is:",key)
+print("value is:",value)
