15001248f73e44171c2a5c17f4029d36b7b2a7db
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-09-10 19:02:07 +0530

    Create pgm2.py

---
+++ pgm2.py
@@ -0,0 +1,17 @@
+students = { "1ms16is100":"asha","1ms16is101":"ashok","1ms16is102":"rekha","1ms16is103":"suma"}
+list = ["value1","value2","value3","value3","value4"]
+list2 = []
+j=0
+#print the student details
+for i in students:
+
+	print ("key is",i,"value is",students[i])
+	list[j]=students[i]
+	#list2[j]=i
+	j=j+1
+print(list)
+
+#print(list2)
+print (students.keys())
+print (students.values())
+print (students.items())
