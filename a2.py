88b55cc7b07e5b01c844b9fe8fda3e21e653c9bc
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-09-10 19:43:29 +0530

    Create a2.py

---
+++ a2.py
@@ -0,0 +1,68 @@
+#task1
+class student:
+    
+    def __init__(self,name,age,m):
+        self.name=name;
+        self.age=age;
+        self.m=m[ : ]
+    
+    def display(self):
+        print("name= ",self.name)
+        print("age= ",self.age)
+        i = 1
+        for mark in self.m:
+            print("mark ", i," = ",mark)
+            i=i+1
+            
+m = []
+def accept():
+    name=input("enter name of student: ")
+    age=input("enter age of student: ")
+    print("enter 3 marks")
+    for i in range(3):
+        m.append(input())
+    return name,age,m
+        
+name,age,m = accept()
+s1 = student(name,age,m)
+
+m = []
+name,age,m = accept()
+s2 = student(name,age,m)
+    
+print(s1.display())
+print()
+print(s2.display())
+#task2
+Sdetails = {
+    10: {
+        "name": "sanj",
+        "age": 20,
+        "sec": "C"
+    },
+    11: {
+        "name": "nive",
+        "age": 20,
+        "sec": "A"
+    },
+    12: {
+        "name": "neha",
+        "age": 21,
+        "sec": "B"
+    },
+    13: {
+        "name": "vish",
+        "age": 20,
+        "sec": "C"
+    },
+    14: {
+        "name": "trivi",
+        "age": 20,
+        "sec": "C"
+    }
+}
+
+for i in Sdetails:
+    if i % 2 == 0:
+        print(i, " : ", Sdetails[i])
+    
