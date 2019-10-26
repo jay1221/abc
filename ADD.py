52adf484732aba5bb762082ac3757c5226a335e9
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-10-26 19:11:34 +0530

    Create ADD.py

---
+++ ADD.py
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title> Simple Calci </title>
+</head>
+<body>
+	<div class="container">
+		<input type="text" placeholder="first number" id="first" />
+		<input type="text" placeholder="first number" id="second" />
+		<div class="btns">
+			<button onClick="multiply()">Multiply</button>
+			<button onClick="add()">ADD</button>
+		</div>
+		<p> Result : <span id="result"></span></p>
+	</div>
+	<script type = "text/javascript">
+		function multiply(){
+        num1 = document.getElementById("first").value;
+        num2 = document.getElementById("second").value;			
+        if(num1 && num2 && num1 > 0 && num2 > 0){
+          document.getElementById("result").innerText = num1 * num2 ;
+        }
+        else{
+          document.getElementById("result").innerHTML = "Please enter a valid positive number" ;	
+        }	
+      }
+		function add(){
+			num1 = parseInt(document.getElementById("first").value);
+			num2 = parseInt(document.getElementById("second").value);			
+			if(num1 && num2 && num1 > 0 && num2 > 0){
+				document.getElementById("result").innerText = num1 + num2 ;
+			}
+			else{
+				document.getElementById("result").innerText = "Please enter a valid positive number" ;	
+			}
+		}	
+	</script>
+</body>
+</html>
+              
