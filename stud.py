be38e86a4539f7a6a64132ac9e35da0a053458f5
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-10-26 14:31:07 +0530

    Update and rename pgm2.py to stud.py

--- stud.py
+++ stud.py
@@ -2,9 +2,9 @@ students = { "1ms16is100":"asha","1ms16is101":"ashok","1ms16is102":"rekha","1ms1
 list = ["value1","value2","value3","value3","value4"]
 list2 = []
 j=0
+
 #print the student details
 for i in students:
-
 	print ("key is",i,"value is",students[i])
 	list[j]=students[i]
 	#list2[j]=i
