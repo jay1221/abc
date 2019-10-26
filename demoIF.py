d216087b0980fde93b6b9d988f02734b21963a46
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-10-26 14:39:40 +0530

    Update demoIF.py

--- demoIF.py
+++ demoIF.py
@@ -2,9 +2,19 @@ x = 0
 
 if x > 0.0:
 	print ("positive")
-
 elif x <0.0:
 	print("negative")
 	x=-1.0*x
 else:
 	print("zero")
+
+	
+#Alternative way
+if x > 0:
+	print("+ve")
+else:
+	if x < 0:
+		print("-ve")
+		x = -1.0*x
+	else:
+		print("0")
