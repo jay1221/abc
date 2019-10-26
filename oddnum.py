273cecd1dd33ce178c32f957c83e8283e30174d6
jay1221 <54971080+jay1221@users.noreply.github.com>
2019-10-26 14:47:04 +0530

    Update oddnum.py

--- oddnum.py
+++ oddnum.py
@@ -13,3 +13,12 @@ def odd(one,two):
 	
 odd(one,two)
 
+#alternate way
+def oddRange(num1, num2):
+	for i in range(num1+1, num2):
+		if i%2 != 0:
+			print(i)
+
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+oddRange(num1, num2)
