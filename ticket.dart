f2e30eaa86c61c3a08cd0aaffd8c86383ce211c9
jay1221 <54971080+jay1221@users.noreply.github.com>
2020-04-22 14:46:48 +0530

    Update ticket.dart

--- ticket.dart
+++ ticket.dart
@@ -1,64 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:metrorun/screens/wrapper.dart';
+import 'package:metrorun/screens/services/home.dart';
+import 'package:metrorun/screens/shared/loading.dart';
+            
+class Home extends StatefulWidget {
+  final Function toggleView;
 
-class Ticket extends StatelessWidget {
+  Home({this.toggleView});
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan[200],
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        title: Text(
-          'TICKET',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25.0,
-          ),
-        ),
-      ),
+    return loading ? Loading() : Scaffold(
+            backgroundColor: Colors.cyan[100],
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              elevation: 5.0,
+              title: Text('TICKET'),
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: <Widget>[
+                    
+                    Center(
+                      child: Text(
+                        'PAYMENT SUCCESSFUL !! 
+                        Scan the QR CODE below : ',
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    )
 
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-          
-            Center(
-              child: Text(
-                'PAYMENT SUCCESSFUL !! 
-                Scan the QR CODE below : ',
-                style: TextStyle(
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w800,
+                    getImageAsset(),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 150.0),
+                      child: ButtonTheme(
+                        minWidth: 200.0,
+                        height: 50.0,
+                        child: RaisedButton(
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(50.0),
+                          ),
+                        elevation: 5.0,
+                        splashColor: Colors.yellowAccent,
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      ),
+                    ),
+            
+                  ],
                 ),
               ),
-            )
-
-            getImageAsset(),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 150.0),
-              child: ButtonTheme(
-                minWidth: 200.0,
-                height: 50.0,
-                child: RaisedButton(
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(50.0),
-                  ),
-                elevation: 5.0,
-                splashColor: Colors.yellowAccent,
-                color: Colors.green,
-                textColor: Colors.white,
-              ),
-            ),
-
-          ],
-        ),
-      ),
-    );
+            ));
   }
 }
-
+                    
 Widget getImageAsset() {
   AssetImage assetImage = AssetImage('assets/QRcode.png');
   Image image = Image(image: assetImage, width: 400.0, height: 400.0,);
@@ -68,3 +77,4 @@ Widget getImageAsset() {
     margin: EdgeInsets.symmetric(vertical: 20.0, horizontal:20.0),
   );
 }
+
